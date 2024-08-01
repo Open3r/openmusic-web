f70369e2e787d317d36a74e491310bdef4d945e8
cher1shRXD <taewoo080401@gmail.com>
2024-08-01 13:39:52 +0900

    feat::mobile exception

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Router from "./components/Router";
 import Header from "./layouts/Header";
 import PlayBar from "./layouts/PlayBar";
 import { isMobile } from "react-device-detect";
+import Logo from "./assets/imgs/logo_color.png";
+import Mock from "./assets/imgs/MockUp.png";
 
 function App() {
   const [headerHideLayout, setHeaderHideLayout] = useState(false);
@@ -12,7 +14,14 @@ function App() {
   const location = useLocation();
 
   useEffect(() => {
-    const hidePaths = ["/login", "/signup", "/verify", "/intro", "/genre", "/callback/google"];
+    const hidePaths = [
+      "/login",
+      "/signup",
+      "/verify",
+      "/intro",
+      "/genre",
+      "/callback/google",
+    ];
     setHeaderHideLayout(hidePaths.includes(location.pathname));
   }, [location.pathname]);
 
@@ -31,28 +40,52 @@ function App() {
 
   return (
     <>
-      {
-        !isMobile ? (
-          <>
-            <CustomToastContainer />
-            <Router />
-            {!headerHideLayout && (
-              <>
-                <Header />
-              </>
-            )}
-            {!playBarHideLayout && (
-              <>
-                <PlayBar />
-              </>
-            )}
-          </>
-        ) : (
-          <h1>모바일 입니다.</h1>
-        )
-      }
+      {!isMobile ? (
+        <>
+          <CustomToastContainer />
+          <Router />
+          {!headerHideLayout && (
+            <>
+              <Header />
+            </>
+          )}
+          {!playBarHideLayout && (
+            <>
+              <PlayBar />
+            </>
+          )}
+        </>
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            width: "100vw",
+            height: "50rem",
+          }}
+        >
+          <img src={Logo} alt="" style={{ marginBottom: "2rem" }} />
+          <h1
+            style={{
+              fontSize: "3rem",
+              marginBottom: "1rem",
+              textAlign: "center",
+            }}
+          >
+            오픈뮤직 웹은
+            <br />
+            PC에서만 지원합니다!
+          </h1>
+          <p style={{ fontSize: "1.7rem", textAlign: "center", color: "gray" }}>
+            모바일에서 오픈 뮤직을 사용하려면
+            <br />
+            모바일 앱을 이용해주세요.
+          </p>
+        </div>
+      )}
     </>
-    
   );
 }
 
